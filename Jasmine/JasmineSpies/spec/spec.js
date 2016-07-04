ef2a72f44aca96c718f93a2791c2eac403a5dfa6
Charlotte Battersby <cbattersby@CBattersby-MLT.local>
2016-07-04 18:51:03 -0400

    created a spy on test with custom built object

--- Jasmine/JasmineSpies/spec/spec.js
+++ Jasmine/JasmineSpies/spec/spec.js
@@ -30,5 +30,36 @@ describe('SpyOn existing functions', function(){
         expect(myObj.getQuantity()).toEqual(10);
 
     });
+    it('should spy on getQuantity fake', function(){
+        var spy = spyOn(myObj, 'getQuantity').and.callFake(function(){
+            console.log('returning 20');
+            return 20;
+        });
+        expect(myObj.getQuantity()).toEqual(20);
+    });
 
+});
+
+describe('Spy on Custom Object', function(){
+    it('should verify arguments', function(){
+        var spy = jasmine.createSpy('mySpy');
+        spy(1);
+        spy(2);
+        spy(1, 1);
+        expect(spy).toHaveBeenCalledWith(1);
+    });
+    it('should verify arguments that werent called', function(){
+        var spy = jasmine.createSpy('mySpy');
+        spy(1);
+        spy(4, 1);
+        spy(4);
+        expect(spy).toHaveBeenCalledWith(4);
+    });
+    xit('should work with metadata', function(){
+        var myObj = spyOn(myObj, "method");
+        myObj.method(1);
+        myObj.method(2);
+        myObj.method(3);
+        expect(spy.calls[0].args[0]).toEqual(1);
+    })
 });
